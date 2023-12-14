2e37d8ba9a9c394ed0d4f2649807b5fd3613ed4a
mariaNoermoelle <cph-mn760@cphbusiness.dk>
2023-12-14 09:05:15 +0100

    Switch case added

--- Exam/Exam/src/main/java/Course.java
+++ Exam/Exam/src/main/java/Course.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Course
 {
     private String courseName;
@@ -7,6 +9,31 @@ public class Course
     {
         this.courseName=courseName;
         this.grade=grade;
+
+        switch (grade)
+        {
+            case -3:
+                System.out.println("-3");
+                break;
+            case 00:
+                System.out.println("00");
+                break;
+            case 02:
+                System.out.println("02");
+                break;
+            case 4:
+                System.out.println("4");
+            case 7:
+                System.out.println("7");
+                break;
+            case 10:
+                System.out.println("10");
+                break;
+            case 12:
+                System.out.println("12");
+            default:
+                System.out.println("Not a grade");
+        }
     }
 
     public String getCourseName()
@@ -14,19 +41,9 @@ public class Course
         return courseName;
     }
 
-/*   public boolean gradeCheck()
-    {
-
-        int grade = 0;
-        if(grade == 12)
-        {
-            System.out.println("well done");
-        }
-        return grade;
-    }
- */
     public double getGrade()
     {
+
         return grade;
     }
 
